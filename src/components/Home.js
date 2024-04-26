29e18d7c2cf489792b06a26d31d1fd78224bc6d0
Tatiana Mathia <mathiatatiana@gmail.com>
2024-04-26 12:31:02 +0300

    jsx lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+    <h1 style={{color : "firebrick"}}></h1>
+    {name} is a web developer from {city}
+  </div>;
 }
 
 export default Home;
